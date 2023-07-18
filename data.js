d43a2b0f25a499b23cbc8068c1d2ec58324eda27
Shaik Subhani <134518091+subhanistark@users.noreply.github.com>
2023-07-18 16:33:34 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+let movies = [
+    {
+        name: "falcon and the winter soldier",
+        des:
+            "Falcon becoming the new Captain America is the coolest thing you can see today,start watching right now.  You cannot imagine a world without Captain America,so now witness sam as the New Captain America.",
+        image: "images/slider 2.PNG"
+    },
+    {
+        name: "loki",
+        des:
+            "Loki from avengers end game is here,witness him right now. The most lovable character left over after endgame, after a heratbreaking sacrifice of Tony Stark ,loki is here to set the timeline in right path.",
+        image: "images/slider 1.PNG"
+    },
+    {
+        name: "wanda vision",
+        des:
+            "After loosing vision in Infinity War, wanda turned into a foe, watch her do things with some crazy past stuff right now,Watch how she created his new vision and took over westview under her control.",
+        image: "images/slider 3.PNG"
+    },
+    {
+        name: "raya and the last dragon",
+        des:
+            "Kids are not listening to you, then its time for you to let your kids have some fun, while watching this amazing movie especially made for kids,They will love this film.",
+        image: "images/slider 4.PNG"
+    },
+    {
+        name: "luca",
+        des:
+            "The coolest animation film not just for kids, watch this with your Family in Snacks time,ave some happy time with the whole Family.",
+        image: "images/slider 5.PNG"
+    }
+];
